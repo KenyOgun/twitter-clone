cf6410325fa2c8a1cffbb2d4072acd2b8a5e97f4
KenyOgun <holleraturboi@yahoo.ca>
2022-07-01 18:53:10 -0400

    Modified tweet.js

--- components/NewTweet.js
+++ components/NewTweet.js
@@ -17,7 +17,15 @@ export default function NewTweet() {
           return;
         }
 
-        alert(content);
+        fetch("/api/tweet", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            content,
+          }),
+        });
       }}
     >
       <div className="flex">

2ad6b5c314ac34d2a5d12aa46e3b65d278dc74bd
KenyOgun <holleraturboi@yahoo.ca>
2022-07-01 14:18:21 -0400

    Modified NewTweet.js and home.js

--- pages/home.js
+++ pages/home.js
@@ -1,47 +1,12 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
 import { useSession } from "next-auth/react";
 import NewTweet from "components/NewTweet";
-import Tweets from "components/Tweets";
-import prisma from "lib/prisma";
-import { getTweets } from "lib/data.js";
-import LoadMore from "components/LoadMore";
 
-export default function Home({ initialTweets }) {
-  const [tweets, setTweets] = useState(initialTweets);
-
-  const router = useRouter();
+export default function Home() {
   const { data: session, status } = useSession();
-  const loading = status === "loading";
 
-  if (loading) {
-    return null;
+  if (status === "loading") {
+    return <p>...</p>;
   }
 
-  if (!session) {
-    router.push("/");
-  }
-
-  if (session && !session.user.name) {
-    router.push("/setup");
-  }
-
-  return (
-    <>
-      <NewTweet tweets={tweets} setTweets={setTweets} />
-      <Tweets tweets={tweets} />
-      <LoadMore tweets={tweets} setTweets={setTweets} />
-    </>
-  );
-}
-
-export async function getServerSideProps() {
-  let tweets = await getTweets(prisma, 2);
-  tweets = JSON.parse(JSON.stringify(tweets));
-
-  return {
-    props: {
-      initialTweets: tweets,
-    },
-  };
+  return <div>{session ? <NewTweet /> : <p>You are not logged in 😞</p>}</div>;
 }
